6a7e2c169231401193e8ad297baeda578890f1e5
Iliya Zayitsev <zayitseviliya@gmail.com>
2025-09-24 16:50:48 +0500

    Change base in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/saa-soft-test/',
+  base: '/typescript-form/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
